af1acef5d15f837f08154612ae8642bb6e7ed0e2
Metal Whale <metalwhale.dev@gmail.com>
2025-01-26 12:40:59 +0900

    Add PostgreSQL client and "Repository" port. Create "news" table. Change "File" entity to "News" entity.

--- chloria-backend/chloria-job/src/execution/tasks/save_news.rs
+++ chloria-backend/chloria-job/src/execution/tasks/save_news.rs
@@ -2,57 +2,90 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use async_trait::async_trait;
+use chrono::{DateTime, Local};
 
 use super::{
     super::ports::{
         file_storage::{FileObjectKind, FileStorage, UploadFileInput},
         http_helper::HttpHelper,
-        news_fetcher::FetchNewsArticle,
+        repository::{InsertNewsInput, Repository},
     },
     LocalTask,
 };
-use crate::domain::file::FileEntity;
+
+pub(in super::super) struct SaveNewsInput {
+    pub(in super::super) source_name: String, // Code name of the source used to fetch the news
+    pub(in super::super) article_id: String,  // Unique article ID for news from the same source
+    pub(in super::super) link: Option<String>, // Link to the original content
+    pub(in super::super) title: Option<String>, // Title of the content
+    pub(in super::super) short_text: Option<String>, // Short description or summary of the content
+    pub(in super::super) long_text: Option<String>, // Full text or detailed content
+    pub(in super::super) image_url: Option<String>, // Url of representative image in the original content
+    pub(in super::super) published_time: Option<DateTime<Local>>, // Date and time when the news was published
+}
 
 pub(in super::super) struct SaveNewsTask {
-    fetch_news_article: FetchNewsArticle,
+    input: SaveNewsInput,
     http_helper: Arc<dyn HttpHelper>,
     file_storage: Arc<dyn FileStorage>,
+    repository: Arc<dyn Repository>,
 }
 
 impl SaveNewsTask {
     pub(in super::super) fn new(
-        fetch_news_article: FetchNewsArticle,
+        input: SaveNewsInput,
         http_helper: Arc<dyn HttpHelper>,
         file_storage: Arc<dyn FileStorage>,
+        repository: Arc<dyn Repository>,
     ) -> Self {
         Self {
-            fetch_news_article,
+            input,
             http_helper,
             file_storage,
+            repository,
         }
     }
 }
 
 #[async_trait(?Send)]
 impl LocalTask for SaveNewsTask {
-    type Output = bool;
+    type Output = Option<i32>;
 
     async fn perform(self) -> Result<Self::Output> {
-        if let Some(image_url) = &self.fetch_news_article.image_url {
-            let file = FileEntity::new(self.fetch_news_article.id, self.fetch_news_article.published_time);
-            let image_bytes = self.http_helper.get(&image_url).await?;
-            let _image_path = self
-                .file_storage
-                .upload_file(UploadFileInput {
-                    kind: FileObjectKind::Origin,
-                    source_name: self.fetch_news_article.source_name,
-                    key: format!("{}.jpg", file.key),
-                    bytes: image_bytes,
-                    created_time: file.created_time,
+        let image_path = match &self.input.image_url {
+            Some(image_url) => {
+                if let Ok(image_bytes) = self.http_helper.get(&image_url).await {
+                    self.file_storage
+                        .upload_file(UploadFileInput {
+                            kind: FileObjectKind::Origin,
+                            source_name: self.input.source_name.clone(),
+                            key: format!("{}.jpg", self.input.article_id),
+                            bytes: image_bytes,
+                            created_time: Local::now(),
+                        })
+                        .await
+                        .ok()
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+        let news_id = tokio::spawn(async move {
+            self.repository
+                .insert_news(InsertNewsInput {
+                    source_name: self.input.source_name,
+                    article_id: self.input.article_id,
+                    link: self.input.link,
+                    title: self.input.title,
+                    short_text: self.input.short_text,
+                    long_text: self.input.long_text,
+                    image_path,
+                    published_time: self.input.published_time,
                 })
-                .await?;
-            return Ok(true);
-        }
-        Ok(false)
+                .await
+        })
+        .await??;
+        Ok(news_id)
     }
 }
