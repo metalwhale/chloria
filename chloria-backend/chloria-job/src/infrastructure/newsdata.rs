aaf658ab38baa575daa0fa4486cb20db97c12e5f
Metal Whale <metalwhale.dev@gmail.com>
2025-01-30 20:58:40 +0900

    Add NewsData client, "NewsFetcher" port and "File" entity.

---
+++ chloria-backend/chloria-job/src/infrastructure/newsdata.rs
@@ -0,0 +1,128 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use chrono::NaiveDateTime;
+use chrono_tz::Tz;
+use log::info;
+use reqwest::Client;
+use serde::Deserialize;
+
+use crate::execution::ports::news_fetcher::{FetchNewsOutput, NewsFetcher};
+
+// Doc: https://newsdata.io/documentation/#http_response
+#[derive(Deserialize)]
+struct NewsResponse {
+    #[serde(rename = "totalResults")]
+    total_results: u16,
+    results: Vec<NewsResponseResult>,
+    #[serde(rename = "nextPage")]
+    next_page: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct NewsResponseResult {
+    article_id: String,
+    link: Option<String>,
+    title: Option<String>,
+    description: Option<String>,
+    image_url: Option<String>,
+    #[serde(rename = "pubDate")]
+    pub_date: Option<String>,
+    #[serde(rename = "pubDateTZ")]
+    pub_date_tz: Option<String>,
+}
+
+pub(crate) struct NewsdataClient {
+    api_key: String,
+    pages_num_limit: Option<u16>,
+}
+
+impl NewsdataClient {
+    pub(crate) fn new(api_key: String, pages_num_limit: Option<u16>) -> Self {
+        Self {
+            api_key,
+            pages_num_limit,
+        }
+    }
+
+    async fn fetch_one_page(&self, page: Option<String>) -> Result<(Vec<FetchNewsOutput>, u16, Option<String>)> {
+        // Doc: https://newsdata.io/news-sources/Japan-news-api
+        let mut url = format!("https://newsdata.io/api/1/latest?country=jp&apikey={}", self.api_key);
+        if let Some(page) = page {
+            url.push_str(&format!("&page={page}"));
+        }
+        let response_text = Client::new().get(url).send().await?.text().await?;
+        let news_response: NewsResponse = serde_json::from_str(&response_text)?;
+        let mut output = vec![];
+        for result in news_response.results.into_iter() {
+            let published_time = if let (Some(pub_date), Some(pub_date_tz)) = (result.pub_date, result.pub_date_tz) {
+                match pub_date_tz.parse::<Tz>()? {
+                    timezone => {
+                        if let Some(published_time) = NaiveDateTime::parse_from_str(&pub_date, "%Y-%m-%d %H:%M:%S")?
+                            .and_local_timezone(timezone)
+                            .single()
+                        {
+                            Some(published_time.to_utc().into())
+                        } else {
+                            None
+                        }
+                    }
+                }
+            } else {
+                None
+            };
+            output.push(FetchNewsOutput {
+                source_code: "NewsData".to_string(),
+                id: Some(result.article_id),
+                link: result.link,
+                title: result.title,
+                short_text: result.description,
+                long_text: None, // `content` field is only available in paid plans
+                image_url: result.image_url,
+                published_time,
+            });
+        }
+        Ok((output, news_response.total_results, news_response.next_page))
+    }
+}
+
+#[async_trait]
+impl NewsFetcher for NewsdataClient {
+    async fn fetch_news(&self) -> Result<Vec<FetchNewsOutput>> {
+        let mut output = vec![];
+        let mut remaining_results_num = None;
+        let mut page = None;
+        let mut page_count = 0;
+        loop {
+            if match self.pages_num_limit {
+                Some(pages_num_limit) => page_count >= pages_num_limit,
+                None => match remaining_results_num {
+                    Some(remaining_results_num) => remaining_results_num <= 0,
+                    None => false,
+                },
+            } {
+                break;
+            }
+            let (mut page_output, total_results, next_page) = self.fetch_one_page(page).await?;
+            info!(
+                "page_count={}, page_output.len={}, total_results={}, next_page={:?}",
+                page_count,
+                page_output.len(),
+                total_results,
+                next_page,
+            );
+            remaining_results_num = Some(
+                match remaining_results_num {
+                    Some(remaining_results_num) => remaining_results_num,
+                    None => total_results,
+                } - page_output.len() as u16,
+            );
+            output.append(&mut page_output);
+            match next_page {
+                Some(next_page) => page = Some(next_page),
+                None => break,
+            };
+            page_count += 1;
+        }
+        Ok(output)
+    }
+}
--- chloria-backend/chloria-job/src/interface/config.rs
+++ chloria-backend/chloria-job/src/interface/config.rs
@@ -1,11 +1,15 @@
-use crate::execution::ports::file_storage::FileStorage;
+use crate::execution::ports::{file_storage::FileStorage, news_fetcher::NewsFetcher};
 
 pub(crate) struct Config<'c> {
+    pub(crate) news_fetcher: &'c dyn NewsFetcher,
     pub(crate) file_storage: &'c dyn FileStorage,
 }
 
 impl<'c> Config<'c> {
-    pub(crate) fn new(file_storage: &'c dyn FileStorage) -> Self {
-        Self { file_storage }
+    pub(crate) fn new(news_fetcher: &'c dyn NewsFetcher, file_storage: &'c dyn FileStorage) -> Self {
+        Self {
+            news_fetcher,
+            file_storage,
+        }
     }
 }
