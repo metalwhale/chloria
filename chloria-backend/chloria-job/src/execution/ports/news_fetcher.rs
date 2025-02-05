3e622f8cc973e58f697f2b03d641a3358db21795
Metal Whale <metalwhale.dev@gmail.com>
2025-02-05 13:37:41 +0000

    Add test to check the required duration for "CollectNewsCase".

--- chloria-backend/chloria-job/src/execution/ports/news_fetcher.rs
+++ chloria-backend/chloria-job/src/execution/ports/news_fetcher.rs
@@ -1,9 +1,12 @@
+use std::{future::Future, pin::Pin};
+
 use anyhow::Result;
 use async_trait::async_trait;
 use chrono::{DateTime, Local};
+use mockall::mock;
 use tokio::task::JoinHandle;
 
-pub(crate) struct FetchNewsOutput {
+pub(crate) struct FetchNewsArticle {
     pub(crate) source_name: String,        // Code name of the source used to fetch the news
     pub(crate) id: Option<String>,         // Unique ID for news from the same source
     pub(crate) link: Option<String>,       // Link to the original content
@@ -14,10 +17,24 @@ pub(crate) struct FetchNewsOutput {
     pub(crate) published_time: Option<DateTime<Local>>, // Date and time when the news was published
 }
 
+pub(crate) type FetchNewsOutput = JoinHandle<Result<()>>;
+pub(crate) type FetchNewsHandler = Box<dyn Fn(FetchNewsArticle) -> FetchNewsOutput + Send>;
+
 #[async_trait]
 pub(crate) trait NewsFetcher {
-    async fn fetch_news(
-        &self,
-        handler: Box<dyn Fn(FetchNewsOutput) -> JoinHandle<Result<()>> + Send>,
-    ) -> Vec<JoinHandle<Result<()>>>;
+    async fn fetch_news(&self, handler: FetchNewsHandler) -> Vec<FetchNewsOutput>;
+}
+
+mock! {
+    pub(in super::super) NewsFetcher {}
+
+    impl NewsFetcher for NewsFetcher {
+        fn fetch_news<'life0, 'async_trait>(
+            &'life0 self,
+            handler: FetchNewsHandler,
+        ) -> Pin<Box<dyn Future<Output = Vec<FetchNewsOutput>> + Send + 'async_trait>>
+        where
+            'life0: 'async_trait,
+            Self: 'async_trait;
+    }
 }
