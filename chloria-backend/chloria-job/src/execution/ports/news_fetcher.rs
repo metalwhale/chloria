3b9061f791890c3ecda9fa48d9428dd9cd038dd6
Metal Whale <metalwhale.dev@gmail.com>
2025-02-24 04:33:29 +0000

    Yahoo client fetches providers simultaneously.

--- chloria-backend/chloria-job/src/execution/ports/news_fetcher.rs
+++ chloria-backend/chloria-job/src/execution/ports/news_fetcher.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, pin::Pin};
+use std::{future::Future, pin::Pin, sync::Arc};
 
 use anyhow::Result;
 use async_trait::async_trait;
@@ -18,23 +18,22 @@ pub(crate) struct FetchNewsArticle {
 }
 
 pub(crate) type FetchNewsOutput = JoinHandle<Result<()>>;
-pub(crate) type FetchNewsHandler = Box<dyn Fn(FetchNewsArticle) -> FetchNewsOutput + Send>;
+pub(crate) type FetchNewsHandler = Arc<dyn Fn(FetchNewsArticle) -> FetchNewsOutput + Send + Sync>;
 
 #[async_trait]
 pub(crate) trait NewsFetcher: Send + Sync {
-    async fn fetch_news(&self, handler: FetchNewsHandler) -> Vec<FetchNewsOutput>;
+    async fn fetch_news(self: Arc<Self>, handler: FetchNewsHandler) -> Vec<FetchNewsOutput>;
 }
 
 mock! {
     pub(in super::super) NewsFetcher {}
 
     impl NewsFetcher for NewsFetcher {
-        fn fetch_news<'life0, 'async_trait>(
-            &'life0 self,
+        fn fetch_news<'async_trait>(
+            self: Arc<Self>,
             handler: FetchNewsHandler,
         ) -> Pin<Box<dyn Future<Output = Vec<FetchNewsOutput>> + Send + 'async_trait>>
         where
-            'life0: 'async_trait,
             Self: 'async_trait;
     }
 }
