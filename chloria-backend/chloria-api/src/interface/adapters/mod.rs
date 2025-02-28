3ee2a023c6d112187a021a2e0464dbbf2fa5fe8e
Metal Whale <metalwhale.dev@gmail.com>
2025-02-28 17:31:04 +0000

    Create "client" tables. Add a route for authentication and a middleware for authorization.

---
+++ chloria-backend/chloria-api/src/interface/adapters/mod.rs
@@ -0,0 +1,21 @@
+pub(super) mod auth;
+
+use serde::Serialize;
+use serde_json::Value;
+
+#[serde_with::skip_serializing_none]
+#[derive(Default, Serialize)]
+pub(super) struct ErrorResponse {
+    code: Option<String>,
+    reason: Option<String>,
+    extra: Option<Value>,
+}
+
+impl From<String> for ErrorResponse {
+    fn from(value: String) -> Self {
+        Self {
+            reason: Some(value),
+            ..Default::default()
+        }
+    }
+}
